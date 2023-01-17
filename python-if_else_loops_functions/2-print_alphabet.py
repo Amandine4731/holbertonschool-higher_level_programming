00eb6c0cbe5099d51d0ad4df125be5709c2ce388
Amandine4731 <5618@holbertonstudents.com>
2023-01-17 05:14:32 -0800

    test task 2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for ascii in range(ord('a'), ord('z')+1):
-    alphabet = chr(ascii)
-    print(f"{alphabet}", end="")
+for alphabet in range(97, 122 +1):
+    print("{:c}".format(alphabet), end="")
