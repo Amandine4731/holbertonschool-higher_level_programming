2334fcec0808e3dc4d92010f2ac8df6c8c9a8ef5
Amandine4731 <5618@holbertonstudents.com>
2023-01-19 01:23:12 -0800

    test task 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add as cal_add
+    import calculator_1 as cal
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, cal_add(a, b)))
-
-    from calculator_1 import sub as cal_sub
-    print("{} - {} = {}".format(a, b, cal_sub(a, b)))
-
-    from calculator_1 import mul as cal_mul
-    print("{} * {} = {}".format(a, b, cal_mul(a, b)))
-
-    from calculator_1 import div as cal_div
-    print("{} / {} = {}".format(a, b, cal_div(a, b)))
+    print("{} + {} = {}".format(a, b, cal.add(a, b)))
+    print("{} - {} = {}".format(a, b, cal.sub(a, b)))
+    print("{} * {} = {}".format(a, b, cal.mul(a, b)))
+    print("{} / {} = {}".format(a, b, cal.div(a, b)))
