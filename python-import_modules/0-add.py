96807980f9c7d0064aa4107efab198a962f76fc4
Amandine4731 <5618@holbertonstudents.com>
2023-01-19 01:01:48 -0800

    test task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0 as cal
-a = 1
-b = 2
 if __name__ == "__main__":
+    import add_0 as cal
+    a = 1
+    b = 2
     print("{} + {} = {}".format(a, b, cal.add(a, b)))
