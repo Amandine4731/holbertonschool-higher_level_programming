4bb159bc129ad0720aec006411eefaef19a0c054
Amandine4731 <5618@holbertonstudents.com>
2023-01-23 02:17:43 -0800

    test task 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in my_list:
-        if idx == 0:
-           return my_list[0]
-        elif idx < 0:
-            return None
-        else:
-            return my_list[i]
+    if idx == 0:
+        return my_list[0]
+    if idx < 0:
+        return None
+    if idx > 0 and idx <= len(my_list):
+        return my_list.pop(idx)
