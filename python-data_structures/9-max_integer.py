1fbcf55c3e889e37a63a73a4975f28b238f58841
Amandine4731 <5618@holbertonstudents.com>
2023-01-24 05:11:36 -0800

    valid task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    for i in my_list:
-        if i > 1:
+
+        if len(my_list) == 0:
+            return None
+
+        if len(my_list) == 1:
+            return my_list[0]
+
+        else:
             my_list.sort()
             my_list.reverse()
-        if i == 0:
-            return None
-    return my_list[0]
+            return my_list[0]
