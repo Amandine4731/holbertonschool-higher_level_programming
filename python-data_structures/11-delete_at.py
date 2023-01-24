1b8e6c28a477631044e86519c1738c8d961d3de7
Amandine4731 <5618@holbertonstudents.com>
2023-01-24 06:30:10 -0800

    test task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    my_list.remove(idx + 1)
-    return my_list
+    if idx < 0 or idx >= len(my_list):
+        return None
+    else:
+        my_list.remove(idx + 1)
+        return my_list
