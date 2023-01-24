47a4bbd38ccccbf8e44c625b8d64e4ec2e1cb3fc
Amandine4731 <5618@holbertonstudents.com>
2023-01-24 03:31:24 -0800

    valid task 6 thanks VAAL34

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-       print(" ".join(map(str, i)))
+    for arr in matrix:
+        i = 0
+        for arr_in_arr in arr:
+            i = i + 1
+            if i == len(matrix):
+                print("{:d}".format(arr_in_arr), end="")
+            else:
+                print("{:d}".format(arr_in_arr), end=" ")
+        print()
