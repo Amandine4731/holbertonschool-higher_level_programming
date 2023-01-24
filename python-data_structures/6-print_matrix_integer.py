b3db24daaf8b00d15168be7ed0367d9d43d0a518
Amandine4731 <5618@holbertonstudents.com>
2023-01-24 03:33:33 -0800

    test valide task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for arr in matrix:
         i = 0
         for arr_in_arr in arr:
-            i = i + 1
+            i += 1
             if i == len(matrix):
                 print("{:d}".format(arr_in_arr), end="")
             else:
