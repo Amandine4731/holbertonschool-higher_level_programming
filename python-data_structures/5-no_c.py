ebae330457205e3ec55e4c293c7a096aeba1be5b
Amandine4731 <5618@holbertonstudents.com>
2023-01-24 01:37:06 -0800

    test task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,9 +2,10 @@
 def no_c(my_string):
     new_list = list(my_string)
     for i in new_list:
-        if i == 'c':
+        if i == 'C':
             new_list.remove(i)
-        elif i == 'C':
+        elif i == 'c':
             new_list.remove(i)
+
     new_list = "".join(new_list)
     return new_list
