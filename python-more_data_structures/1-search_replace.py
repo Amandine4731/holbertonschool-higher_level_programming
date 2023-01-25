a6c463c76bbda0282171f9dfe9d8ed3aab0c9910
Amandine4731 <5618@holbertonstudents.com>
2023-01-25 06:58:22 -0800

    test task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,7 +3,10 @@ def search_replace(my_list, search, replace):
     for i in range(len(my_list)):
         if my_list != 0:
             if my_list[i] == search:
-                my_list[i] = replace
+                if replace == 0:
+                    my_list[i] = 0
+                else:
+                    my_list[i] = replace
         else:
             return None
     return my_list
