3eb040f0924b4958b1295f096730f4934f9bc79b
mandinou <113857342+Amandine4731@users.noreply.github.com>
2023-01-25 10:45:18 +0100

    valid task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_list = list(my_string)
-    for i in new_list:
+    new_list = ""
+    for i in my_string:
         if i == 'C':
-            new_list.remove(i)
+            continue
         elif i == 'c':
-            new_list.remove(i)
+            continue
 
-    new_list = "".join(new_list)
+    new_list += i
     return new_list
