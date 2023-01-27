bc2d30dd270be4e646c6c069d99bb142a080519d
Amandine4731 <5618@holbertonstudents.com>
2023-01-27 06:03:19 -0800

    test task 3

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        if b != 0 or a != 0:
+            div = a / b          
+
+    except ZeroDivisionError:
+        div = None
+
+    finally:
+        print("Inside result: {}".format(div))
+        return div
