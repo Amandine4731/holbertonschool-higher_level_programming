d11d3a691b97878e60a96ff348850af121382c86
Amandine4731 <5618@holbertonstudents.com>
2023-01-27 06:09:53 -0800

    valid task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        if b != 0 or a != 0:
-            div = a / b     
+        div = a / b     
 
     except ZeroDivisionError:
         div = None
