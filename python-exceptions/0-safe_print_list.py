de6e38c6bb61df17a872bc0b84c3999685979c3c
Amandine4731 <5618@holbertonstudents.com>
2023-01-27 01:25:24 -0800

    test task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -8,11 +8,13 @@ def safe_print_list(my_list=[], x=0):
                 print(i, end='')
             print()
             return x
-        else:
+        elif x not in my_list and x > 0:
             while i < max(my_list):
                 i += 1
                 print(i, end='')
             print()
             return max(my_list)
+        else:
+            return 0
     except:
         print("empty list")
