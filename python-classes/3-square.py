8f58db18d3b9511b82f5ce0bb185c5386b5755bb
Amandine4731 <5618@holbertonstudents.com>
2023-01-30 06:29:20 -0800

    test task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python program to create empty class (square)
+"""
+
+
+class Square:
+    """ private class attribute """
+    def __init__(self, size=0):
+        """ private instance attribute """
+        self.__size = size ** 2
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """ return operation square """
+        return self.__size
