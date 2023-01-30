43d0d38f97ba2bff70ba976edbe394065f17c7ac
Amandine4731 <5618@holbertonstudents.com>
2023-01-30 15:04:43 -0800

    test task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -53,9 +53,12 @@ class Square:
         """ to print in stdout the square with the character # """
         if self.__size == 0:
             print()
+        for pos1 in range(self.__position[1]):
+            print()
         for i in range(self.__size):
-            for pos in range(self.__position[0]):
+            for pos2 in range(self.__position[0]):
                 print(end=" ")
+
             for j in range(self.__size):
                 print("#", end='')
             print()
