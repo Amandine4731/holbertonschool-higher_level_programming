efa121503dcba8b69039b9cbf859573a582e2baf
Amandine4731 <5618@holbertonstudents.com>
2023-01-30 02:21:20 -0800

    test task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Python program to create empty class (square)
+"""
+
+
+class Square:
+    """ private class attribute """
+    __size = {} 
+    
+    def __init__(self, __size):
+        """ private instance attribute """
+        self.__size = __size
+
+    def __display(self):
+        """ print square """
+        print(f"'Square' object has no attribute {self.__size}")
