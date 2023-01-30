6db95a2c4afe86c3d731584f40eced64cb187fef
Amandine4731 <5618@holbertonstudents.com>
2023-01-30 14:46:19 -0800

    test task 6

---
+++ python-classes/6-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+Python program to create empty class (square)
+"""
+
+
+class Square:
+    """ private class attribute """
+    def __init__(self, size=0, position=(0, 0)):
+        """ private instance attribute """
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """ property to retrieve it (size) """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ property to set it (size) """
+        self.__size = value
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def position(self):
+        """ property to retrieve it (position) """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """ property to set it (position) """
+        self.__position = value
+
+        if type(value) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        if value < 0:
+            raise ValueError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """ return operation square """
+        return self.__size ** 2
+
+    def my_print(self):
+        """ to print in stdout the square with the character # """
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for pos in range(self.__position[0]):
+                print(end=" ")
+            for j in range(self.__size):
+                print("#", end='')
+            print()
