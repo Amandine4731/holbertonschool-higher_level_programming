f2dd4aaccd51a9404e6217f2b346c38abd61450c
Amandine4731 <5618@holbertonstudents.com>
2023-02-01 06:14:07 -0800

    test task 1

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    function to divide all elements of a matrix
+"""
+
+
+def matrix_divided(matrix, div):
+    """ function to divide all elements of a matrix """
+    size = len(matrix[0])
+    try:
+        new_matrix = []
+        final_matrix = []
+        for arr in matrix:
+            new_matrix = []
+            for arr_in_arr in arr:
+                new_matrix.append(round(float(arr_in_arr / div), 2))
+            final_matrix.append(new_matrix)
+        return final_matrix
+
+
+    except TypeError:
+        return "matrix must be a matrix (list of lists) of integers/floats"
+
+    except ZeroDivisionError:
+        if div == 0:
+            return "division by zero"
