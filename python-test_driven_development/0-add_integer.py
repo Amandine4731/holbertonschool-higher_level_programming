d3154082496b89540f7237f1e8773d321298c0d1
Amandine4731 <5618@holbertonstudents.com>
2023-02-01 04:57:05 -0800

    test task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+    function to add two integers
+"""
+
+
 def add_integer(a, b=98):
-    """ function to add two integers """
     try:
         result = a + b
         return int(result)
