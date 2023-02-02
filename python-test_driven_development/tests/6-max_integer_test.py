80011d3294eb639849e175527709a63225883787
Amandine4731 <5618@holbertonstudents.com>
2023-02-02 08:59:01 -0800

    test task 5

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,5 +6,26 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    def max(self):
+    def test_max(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_maxattheend(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_maxatthebeginning(self):
+        self.assertEqual(max_integer([4, 2, 3, 1]), 4)
+
+    def test_maxatthemiddle(self):
+        self.assertEqual(max_integer([1, 4, 3, 2]), 4)
+
+    def test_onenegnum(self):
+        self.assertEqual(max_integer([1, 2, -3, 4]), 4)
+
+    def test_onlynegnum(self):
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_listofoneelement(self):
+        self.assertEqual(max_integer([4]), 4)
+
+    def test_listempty(self):
+        self.assertEqual(max_integer(), None)
