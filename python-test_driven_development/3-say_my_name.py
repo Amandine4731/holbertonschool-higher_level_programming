e39ed41a2d0fa167f3a9100b1c88f462fc3a4a99
Amandine4731 <5618@holbertonstudents.com>
2023-02-02 04:40:47 -0800

    test task 2

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -7,12 +7,10 @@
 def say_my_name(first_name, last_name=""):
     """ function to print name """
 
-    try:
-        if type(first_name) is str and type(last_name) is str:
-            print("My name is {} {}".format(first_name, last_name))
 
-    except TypeError:
-        if type(first_name) is not str:
-            return "first_name must be a string"
-        elif type(last_name) is not str:
-            return "last_name must be a string"
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    elif not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
