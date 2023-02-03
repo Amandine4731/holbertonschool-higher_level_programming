05e6d7f897e1d6bd287569c80899d897ac336e53
Amandine4731 <5618@holbertonstudents.com>
2023-02-03 05:37:01 -0800

    test task 6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -6,13 +6,14 @@ Python program to create empty class that defines a rectangle
 
 class Rectangle:
     """ private class attribute """
-    
+
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
+        Rectangle.number_of_instances += 1
         """ private instance attribute """
         self.height = height
         self.width = width
-    number_of_instances += 1
 
     @property
     def height(self):
@@ -45,14 +46,12 @@ class Rectangle:
     def area(self):
         """ return operation area """
         return self.__height * self.__width
-    number_of_instances += 1
 
     def perimeter(self):
         """ return operation perimeter """
         if self.__height == 0 or self.__width == 0:
             return 0
         return (self.__height + self.__width) * 2
-    number_of_instances += 1
 
     def __str__(self):
         """ Instance method to return an informal printable string """
@@ -66,13 +65,12 @@ class Rectangle:
                 if i < self.__height - 1:
                     string = string + '\n'
             return string
-    number_of_instances += 1
 
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
         del(self.__width, self.__height)
         return
-    number_of_instances -= 1
