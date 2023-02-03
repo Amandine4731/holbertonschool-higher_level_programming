4d6687624f14b7a442f66eaf8accffa05257959f
Amandine4731 <5618@holbertonstudents.com>
2023-02-03 01:33:21 -0800

    test task 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+Python program to create empty class that defines a rectangle
+"""
+
+
+class Rectangle:
+    """ private class attribute """
+
+    def __init__(self, width=0, height=0):
+        """ private instance attribute """
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    def area(self):
+        """ return operation area """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """ return operation perimeter """
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return (self.__height + self.__width) * 2
