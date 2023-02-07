e757b58ab8f182622df3d2931b63d9efb683796e
Amandine4731 <5618@holbertonstudents.com>
2023-02-07 04:32:51 -0800

    test task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+Python program to return a list of object
+"""
+
+
+class BaseGeometry:
+    """ class BaseGeometry """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif int(value) <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """ class Rectangle inherits from BaseGeometry """
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
