5ffc42c05d48900c6368bee07c7cc89aab5a8988
Amandine4731 <5618@holbertonstudents.com>
2023-02-07 01:46:22 -0800

    test task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+Python program to return true if oject is an instance
+"""
+
+
+def inherits_from(obj, a_class):
+    """ true if oject is an instance or a class that inherited from """
+    if type(obj) is a_class:
+        return False
+    elif isinstance(obj, a_class):
+        return True
+    else:
+        return False
