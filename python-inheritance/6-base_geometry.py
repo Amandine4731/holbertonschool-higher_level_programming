a4791713f39f78ea177826a85e0e3fd2c1f79fc0
Amandine4731 <5618@holbertonstudents.com>
2023-02-07 01:56:01 -0800

    test task 6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -6,6 +6,6 @@ Python program to return a list of object
 
 class BaseGeometry:
     """ class with public instance method """
-    
+
     def area(self):
         raise Exception("area() is not implemented")
