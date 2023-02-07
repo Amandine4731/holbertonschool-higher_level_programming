b864edf7e6632c338d04f2235340a21173ec8378
Amandine4731 <5618@holbertonstudents.com>
2023-02-07 02:08:41 -0800

    test task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+Python program to return a list of object
+"""
+
+
+class BaseGeometry:
+    """ class with public instance method """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif int(value) <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
