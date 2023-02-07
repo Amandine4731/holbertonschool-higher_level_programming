29ae88842b9ef46aef8b42fe8a3dcf48806507a7
Amandine4731 <5618@holbertonstudents.com>
2023-02-07 01:43:35 -0800

    test task 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,7 +5,7 @@ Python program to return true if oject is an instance
 
 
 def is_kind_of_class(obj, a_class):
-    """ function to return true if oject is an instance or a class that inherited from """
+    """ true if oject is an instance or a class that inherited from """
     if type(obj) is a_class:
         return True
     elif isinstance(obj, a_class):
