5f703445e71ca10f8006ffc03ae8f50d8b73d320
Amandine4731 <5618@holbertonstudents.com>
2023-02-07 01:22:31 -0800

    test task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,7 +6,6 @@ Python program to create a list that inherits from an other list
 
 class MyList(list):
     """ private class attribute """
-    
 
     def print_sorted(self):
         """ print the list sorted """
