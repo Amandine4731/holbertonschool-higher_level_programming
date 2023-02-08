16fb5db88e9abaddc1fb0377d40af1d1a0ad8c60
Amandine4731 <5618@holbertonstudents.com>
2023-02-08 07:12:29 -0800

    test task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+    Python program
+"""
+
+
+def write_file(filename="", text=""):
+    """ read a text file """
+    with open(filename, encoding="utf-8") as f:
+        f.write(text)
