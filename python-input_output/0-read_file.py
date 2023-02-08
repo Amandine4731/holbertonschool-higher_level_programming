65d2f6eee77d149b3f1bd14191147be889322d93
Amandine4731 <5618@holbertonstudents.com>
2023-02-08 06:40:05 -0800

    test task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+    Python program
+"""
+
+
+def read_file(filename=""):
+    """ read a text file """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
