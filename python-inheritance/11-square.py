14c353e9b6388f6b36c61793f657e216476b5be1
Amandine4731 <5618@holbertonstudents.com>
2023-02-08 01:58:32 -0800

    test task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+Python program to return a list of object
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ class Square inherits from Rectangle """
+
+    def __init__(self, size):
+        self.__size = size
+
+        self.__size == BaseGeometry.integer_validator(self, 'size', size)
+
+    def area(self):
+        """ return operation area """
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
