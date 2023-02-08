e28920503904698667d8b848c93e904008f3535e
Amandine4731 <5618@holbertonstudents.com>
2023-02-08 01:52:12 -0800

    test task 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+Python program to return a list of object
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ class Rectangle inherits from BaseGeometry """
+
+    def __init__(self, width, height):
+        self.__height = height
+        self.__width = width
+
+        self.__height == BaseGeometry.integer_validator(self, 'height', height)
+        self.__width == BaseGeometry.integer_validator(self, 'width', width)
+
+    def area(self):
+        """ return operation area """
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
