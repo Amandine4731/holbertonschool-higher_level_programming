b5220127e90e21e923c5658375fc18bb17e99bc5
Amandine4731 <5618@holbertonstudents.com>
2023-02-09 00:38:10 -0800

    test task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,7 +6,7 @@
 
 def append_write(filename="", text=""):
     """ read a text file """
-    with open(filename, 'w+', encoding="utf-8") as f:
+    with open(filename, 'a+', encoding="utf-8") as f:
         f.read()
         f.write(text)
 
