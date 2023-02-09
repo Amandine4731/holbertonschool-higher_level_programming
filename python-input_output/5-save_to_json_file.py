36e8a222f0f095e3b3f9faeea2963c45c3e9abdb
Amandine4731 <5618@holbertonstudents.com>
2023-02-09 01:17:04 -0800

    test task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+    Python program
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ return an object represented by a JSON string """
+    with open(filename, 'w') as f:
+        return json.dumps(my_obj)
