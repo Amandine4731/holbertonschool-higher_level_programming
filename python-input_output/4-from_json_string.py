fe4ee2b070bc6b8c0bb99a02cde019096b9c0bcf
Amandine4731 <5618@holbertonstudents.com>
2023-02-09 01:00:30 -0800

    test task 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+    Python program
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """ return an object represented by a JSON string """
+    return json.loads(my_str)
