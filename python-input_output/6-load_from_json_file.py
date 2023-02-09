641d25cef03660bd5fba5428689856ff126c7565
Amandine4731 <5618@holbertonstudents.com>
2023-02-09 01:58:39 -0800

    test task 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+    Python program
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """ create an Object from a "JSON file" """
+    with open(filename, 'r') as f:
+        f.read(filename)
+    f.close()
+    return(json.loads(filename))
