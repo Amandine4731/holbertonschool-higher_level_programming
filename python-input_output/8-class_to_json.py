7ed3dfde21d20e5cad5d157d06ae5633fe434995
Amandine4731 <5618@holbertonstudents.com>
2023-02-10 02:38:56 -0800

    test task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+    Python program
+"""
+
+
+import json
+
+def class_to_json(obj):
+    """  return the dictionary description with simple data structure """
+    return json.dumps(obj.__dict__)
