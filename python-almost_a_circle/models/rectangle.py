bf16ff514dc2be078b723690036af48af61670c0
Amandine4731 <5618@holbertonstudents.com>
2023-02-14 00:54:02 -0800

    valid task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -18,10 +18,38 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
+    """ width """
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, new_width):
         self.__width = new_width
+
+    """ height """
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, new_height):
+        self.__width = new_height
+
+    """ x """
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, new_x):
+        self.__width = new_x
+
+    """ y """
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, new_y):
+        self.__width = new_y
