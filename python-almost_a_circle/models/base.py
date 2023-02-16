655e5c81636cb1a37e5950a88f8b0821162471cf
Amandine4731 <5618@holbertonstudents.com>
2023-02-16 00:48:00 -0800

    test task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 Python program
 """
 
+import json
+
 
 class Base:
     """ base class created """
@@ -15,3 +17,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ return json string """
+        return json.dumps(list_dictionaries)
