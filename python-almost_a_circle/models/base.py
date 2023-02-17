c8aa02e85193147e9e885a88dfd0e772ff467eff
Amandine4731 <5618@holbertonstudents.com>
2023-02-17 05:04:53 -0800

    test task 18

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -50,6 +50,9 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """ return an instance with all attributes already set """
-        dummy = cls(1, 1)
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
