34869180f9ea00c2122666de8ea4a07536aadd97
Amandine4731 <5618@holbertonstudents.com>
2023-03-13 05:03:47 -0700

    try to create a table in my database with database and table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -3,8 +3,8 @@
 -- The database name will be passed as an argument of the mysql command.
 -- If the table first_table already exists, your script should not fail.
 -- You are not allowed to use the SELECT or SHOW statements.
-CREATE TABLE IF NOT EXISTS hbtn_test_db_4.first_table
+CREATE TABLE IF NOT EXISTS first_table
 (
-    id INT() NOT NULL,
-    name VARCHAR(256) NOT NULL
+    id INT,
+    name VARCHAR(256)
 );
