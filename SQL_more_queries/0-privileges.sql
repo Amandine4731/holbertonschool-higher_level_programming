4015a8069627c9c1478f5ac299b3d55221cc65e9
Amandine4731 <5618@holbertonstudents.com>
2023-03-14 02:11:29 -0700

    list all privileges user and host

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-SHOW GRANTS;
+SELECT User, Host FROM user_0d_1;
+SELECT User, Host FROM user_0d_2;
