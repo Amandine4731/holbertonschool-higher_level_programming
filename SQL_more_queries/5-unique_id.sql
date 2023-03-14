07334e0cf87d5a392b9e2a441bab868cbbcf8e22
Amandine4731 <5618@holbertonstudents.com>
2023-03-14 02:53:49 -0700

    create new table with uniques id

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -8,7 +8,6 @@
 -- If the table unique_id already exists, your script should not fail
 CREATE TABLE IF NOT EXISTS unique_id
 (
-    id INT(1) NOT NULL,
+    id INT(1) UNIQUE,
     name VARCHAR(256)
-    UNIQUE (id)
 );
