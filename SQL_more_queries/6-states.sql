4dad19de115ef8d2b9e4532fc643de4f2127f79a
Amandine4731 <5618@holbertonstudents.com>
2023-03-14 03:10:58 -0700

    create new database, table with id INT unique, auto generated, can’t be null and is a primary key

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -7,8 +7,8 @@
 -- If the database hbtn_0d_usa already exists, your script should not fail
 -- If the table states already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states FROM hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
 (
-    id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
