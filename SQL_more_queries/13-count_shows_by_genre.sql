b839b8a7b48887f9c15bc62b3b08b4e538467cd2
Amandine4731 <5618@holbertonstudents.com>
2023-03-15 09:51:51 -0700

    count the number of shows in all genres with group by

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -11,6 +11,6 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_genres.name AS genre, SUM(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_show_genres.genre_id
 ORDER BY SUM(tv_show_genres.genre_id) DESC;
