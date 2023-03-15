146d81009e14a013568e824a3180dde44433331a
Amandine4731 <5618@holbertonstudents.com>
2023-03-15 01:40:09 -0700

    try to put a select in a select)

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,8 +3,5 @@
 -- Results must be sorted in ascending order by cities.id
 -- You are not allowed to use the JOIN keyword
 -- The database name will be passed as an argument of the mysql command"
-SELECT name FROM hbtn_0d_usa.states WHERE name = "California"
-ORDER BY id DESC;
-
-SELECT name FROM hbtn_0d_usa.cities WHERE name = "California"
-ORDER BY id DESC;
+SELECT id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California')
+ORBER BY cities.id;
