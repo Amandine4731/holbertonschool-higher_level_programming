b7f445d6db8a039a109823220b9d0d4ac351b15f
Amandine4731 <5618@holbertonstudents.com>
2023-03-16 03:09:25 -0700

    name and title only dexter

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -7,9 +7,10 @@
 -- Results must be sorted in ascending order by the genre name
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT tv_shows.title, tv_genres.name
+SELECT tv_genres.name
 FROM tv_show_genres
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 AND title = 'Dexter'
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
 ORDER BY tv_genres.name DESC;
