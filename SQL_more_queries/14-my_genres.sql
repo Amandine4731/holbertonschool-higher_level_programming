58742161d31c60766cc04a494a1b9441268f02a0
Amandine4731 <5618@holbertonstudents.com>
2023-03-16 03:10:15 -0700

    name and title only dexter

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -13,4 +13,4 @@ INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 AND title = 'Dexter'
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
-ORDER BY tv_genres.name DESC;
+ORDER BY tv_genres.name;
