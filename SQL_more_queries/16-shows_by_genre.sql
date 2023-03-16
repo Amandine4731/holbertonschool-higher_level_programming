9e9da27dd677c014858499d1200fc7d814fba23d
Amandine4731 <5618@holbertonstudents.com>
2023-03-16 03:30:11 -0700

    name and title and null

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -11,5 +11,4 @@ SELECT tv_shows.title, tv_genres.name
 FROM tv_show_genres
 RIGHT OUTER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 LEFT OUTER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_shows.title, tv_genres.name
 ORDER BY tv_shows.title;
