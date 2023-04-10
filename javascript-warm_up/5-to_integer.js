611e6ecc9eddea281823698cdfe6eb8902dffcb1
Amandine4731 <5618@holbertonstudents.com>
2023-04-10 02:09:17 -0700

    test

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const args = process.argv;  // comment
+const args = process.argv;
 
-if (0 < args[2] > 0) {
+if (0 > args[2] > 0) {
   console.log(('My number: ' + Number(~~args[2])));
 } else if ((args[1] && !args[2]) || (args[1] && args[2] !== Number)) {
   console.log('Not a number');
