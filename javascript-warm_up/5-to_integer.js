48eefcb26c5e69054739305f50f9048524ff0db1
Amandine4731 <5618@holbertonstudents.com>
2023-04-10 01:50:48 -0700

    only number

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,8 +2,8 @@
 
 const args = process.argv;
 
-if ((args[1] && !args[2]) || (args[1] && args[2] !== Number)) {
+if (args[2] >= 0) {
+  console.log(('My number: ' + Number(~~args[2])));
+} else if ((args[1] && !args[2]) || (args[1] && args[2] !== Number)) {
   console.log('Not a number');
-} else if (args[2]) {
-  console.log('My number: ' + Number(~~args[2]));
 }
