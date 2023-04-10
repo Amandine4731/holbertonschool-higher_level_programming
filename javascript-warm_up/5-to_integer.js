dcf1c41c2e4188e67d469270bb599f5b69220909
Amandine4731 <5618@holbertonstudents.com>
2023-04-10 02:16:11 -0700

    good code

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const args = process.argv;
 
-if (args[2] > 0 || args[2] < 0) {
+if (args[2] > 0 || args[2] < 0 || args[2] == 0) {
   console.log(('My number: ' + Number(~~args[2])));
 } else if ((args[1] && !args[2]) || (args[1] && args[2] !== Number)) {
   console.log('Not a number');
