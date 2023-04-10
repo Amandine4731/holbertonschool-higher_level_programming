16be5491a0fa4ea69c6f905b32ecb5fe38e172b9
Amandine4731 <5618@holbertonstudents.com>
2023-04-10 10:09:40 -0700

    print matrice with number of the first argument

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+
+const x = argv[2];
+const y = argv[2];
+
+if (argv[2] >= 0 || argv[2] < 0) {
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < y - 1; j++) {
+      process.stdout.write('x');
+    }
+    process.stdout.write('x');
+    console.log('');
+  }
+} else if (argv[2] !== Number) {
+  console.log('Missing size');
+}
