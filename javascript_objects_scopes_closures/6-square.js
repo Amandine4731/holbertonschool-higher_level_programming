f2c46e55690659c3468cc973aafa44fefe1fb1bf
Amandine4731 <5618@holbertonstudents.com>
2023-04-12 01:19:38 -0700

    change the name of the variable class

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,35 +1,8 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const Squareparent = require('./5-square');
 
-  print () {
-    for (let j = 0; j < this.height; j++) {
-      for (let i = 0; i < this.width; i++) {
-        process.stdout.write('X');
-      }
-      console.log();
-    }
-  }
-
-  rotate () {
-    const stock = this.height;
-    this.height = this.width;
-    this.width = stock;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
-
-class Square extends Rectangle {
+class Square extends Squareparent {
   constructor (size) {
     super(size, size);
   }
@@ -47,5 +20,4 @@ class Square extends Rectangle {
     }
   }
 }
-module.exports = Rectangle;
 module.exports = Square;
