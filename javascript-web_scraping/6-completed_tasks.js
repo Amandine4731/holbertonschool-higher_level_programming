419ec8911504d8729a7456f5a62657bc6a972fa7
Amandine4731 <5618@holbertonstudents.com>
2023-04-18 05:56:42 -0700

    test with -1

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -14,7 +14,7 @@ request(opts, (err, res, body) => {
     const parse = JSON.parse(body);
     let result = 0;
     let user = 1;
-    for (user; user < parse.length; user++) {
+    for (user; user < parse.length - 1; user++) {
       result = 0;
       for (const i of parse) {
         if (i.userId === user) {
@@ -30,47 +30,3 @@ request(opts, (err, res, body) => {
     console.log(newDict);
   }
 });
-/*
-#!/usr/bin/node
-
-const request = require('request');
-const argv = process.argv;
-const opts = {
-  method: 'GET',
-  url: argv[2]
-};
-
-request(opts, (err, res, body) => {
-  if (err) {
-    console.log('Error: ', err);
-  } else {
-    const parse = JSON.parse(body);
-    let result = 0;
-    for (const i of parse.results) {
-      for (const j of i.characters) {
-        if (j.includes(urlId)) {
-          result += 1;
-        }
-      }
-    }
-    console.log(result);
-  }
-});
-
-#!/usr/bin/node
-
-const request = require('request');
-const argv = process.argv;
-const uniqueId = Number(argv[2]);
-const opts = {
-  method: 'GET',
-  url: 'https://swapi-api.hbtn.io/api/films/' + uniqueId
-};
-
-request(opts, (err, res, body) => {
-  if (err) return console.log('Error: ', err);
-
-  console.log(JSON.parse(body).title);
-});
-
-*/
