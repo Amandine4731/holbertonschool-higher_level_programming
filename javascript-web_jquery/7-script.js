b7673d41ec4d05bc0f4bdd23e9e023bbc714a774
Amandine4731 <5618@holbertonstudents.com>
2023-04-21 04:39:10 -0700

    fetch key from url

--- javascript-web_jquery/7-script.js
+++ javascript-web_jquery/7-script.js
@@ -1,3 +1,3 @@
-$.getJSON('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
-  $('DIV#character').html(data.name);
-});
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function(data) {
+  $('DIV#character').text(data.name);
+}); 
